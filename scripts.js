a42d903e303cec915bce75547d59ca92b14f2238
Alexis Pendleton <alexispendleton@outlook.com>
2022-10-10 09:18:55 +0100

    sdsd

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const game = document.getElementById('game')
+
+const scoreDisplay = document.getElementById('score')
+
+const questionCatergories = [
+    {
+        subjects: "Science",
+        questions: []
+    },
+    {
+        subjects: "HTML",
+        questions: []
+    },
+    {
+        subjects: "CSS",
+        questions: []
+    },
+    {
+        subjects: "Science",
+        questions: []
+    },
+    {
+        subjects: "Science",
+        questions: []
+    },
+]
+}
